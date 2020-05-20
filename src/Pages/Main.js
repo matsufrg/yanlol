d3bf34ca335269d5d05be537fba64b724794476f
Roberto <matsufrg@gmail.com>
2020-05-20 11:40:30 -0300

    mudanças no cors

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -22,8 +22,15 @@ export default class Main extends Component {
 
     let matches;
 
+    const config = {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Access-Control-Allow-Origin": "*"
+      }
+    }
+
     if (!localStorage.getItem('match')) {
-      const matchList = await axios.get(`https://cors-anywhere.herokuapp.com/https://br1.api.riotgames.com/lol/match/v4/matchlists/by-account/${this.state.userId}?endIndex=18&beginIndex=0&api_key=RGAPI-93c7a000-ba7e-4aa0-9e79-2887623b68a5`)
+      const matchList = await axios.get(`https://br1.api.riotgames.com/lol/match/v4/matchlists/by-account/${this.state.userId}?endIndex=18&beginIndex=0&api_key=RGAPI-93c7a000-ba7e-4aa0-9e79-2887623b68a5`, config);
 
       matches = await this.getMatches(matchList);
 
